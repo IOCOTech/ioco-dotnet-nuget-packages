b9523b3e6292180eb1e3ad0f43c60f81295a13fa
Oluwafemi Otusanya <114735841+femi-eoh@users.noreply.github.com>
2022-10-18 06:10:22 +0200

    Vault Package Solution

---
+++ Ioco.Infrastructure.Vault/Ioco.Infrastructure.Vault/ModuleInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+
+namespace Ioco.Infrastructure.Vault
+{
+    public static class ModuleInitializer
+    {
+        public static IConfigurationBuilder AddVaultSupport(this IConfigurationBuilder configHost)
+    {
+        var builtConfig = configHost.Build();
+
+        var vault = builtConfig["KeyVault"];
+
+        if (!string.IsNullOrWhiteSpace(vault))
+        {
+            var secretClient = new SecretClient(new Uri(vault), new DefaultAzureCredential());
+            configHost.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+        }
+
+        return configHost;
+    }
+    }
+}
